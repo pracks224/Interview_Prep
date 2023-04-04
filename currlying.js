8f49cf42b69f6cc601a9cb24974cf31024cf7931
pracks224 <p.sh.433@gmail.com>
2023-04-04 16:09:29 +0530

    Update currlying.js

--- currlying.js
+++ currlying.js
@@ -8,3 +8,12 @@ function sum(x) {
   };
 }
 console.log(sum(2)(3)())
+
+//Using Arrow function
+const sum = x=>y=>z=>{
+    if(z)
+      return sum(x+y)(z);
+    else
+       return x+y;
+}
+console.log(sum(2)(3)(5)())
